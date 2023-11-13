d81c04d45fa039c2ed901a41cdcde9cca18c4292
Claudia Villarroel <claudia.villarroel@live.com>
2023-11-13 17:33:10 -0300

    :zap: Configuracion tema

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html", "./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./src/**/*.{html,js}", "./public/*.{html,js}"],
+	theme: {
+		fontFamily: {
+			Montserrat: ['"Montserrat"', "sans-serif"],
+		},
+    backgroundColor: (theme) => ({
+      ...theme("colors"),
+      primary: "#CC2D4A",
+      secondary: "#8FA206",
+      tertiary: "#61AEC9",
+    }),
+		colors: {
+			primary: "#CC2D4A",
+			secondary: "#8FA206",
+			tertiary: "#61AEC9",
+		},
+		extend: {
+			sanFrancisco: "url('../img/sanFrancisco.jpg')",
+			sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+			yosemite: "url('../img/yosemite.jpg')",
+			LA: "url('../img/LA.jpg')",
+			seattle: "url('../img/seattle.jpg')",
+			new_york: "url('../img/new_york.jpg')",
+			norway: "url('../img/norway.jpg')",
+			sydney: "url('../img/sydney.jpg')",
+			miami: "url('../img/miami.jpg')",
+			switzerland: "url('../img/switzerland.jpg')",
+			bali: "url('../img/bali.jpg')",
+			norway: "url('../img/norway.jpg')",
+			chicago: "url('../img/chicago.jpg')",
+			europe: "url('../img/europe.jpg')",
+			iceland: "url('../img/iceland.jpg')",
+		},
+	},
+	plugins: [
+		require("@tailwindcss/forms"),
+		require("@tailwindcss/typography"),
+		require("@tailwindcss/aspect-ratio"),
+	],
+};
